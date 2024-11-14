470fccffe3ec3a6aee7e054df30ce47ea2209569
Wilczusia <furrythebest200@gmail.com>
2024-11-14 20:19:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function checkPassword() {
-    const input = document.getElementById("password").value;
-    const secretContent = document.getElementById("secret-content");
-    const correctPassword = "sha'rathra";
+    const correctPassword = "sha'rathra"; // Ustaw swoje hasło tutaj
+    const enteredPassword = document.getElementById("password").value;
+    const errorMessage = document.getElementById("error-message");
 
-    if (input === correctPassword) {
-        secretContent.classList.remove("hidden");
+    if (enteredPassword === correctPassword) {
+        window.location.href = "book.html"; // Przekierowanie do strony księgi
     } else {
-        alert("Niepoprawne hasło! Spróbuj ponownie.");
+        errorMessage.style.display = "block"; // Wyświetl komunikat o błędzie
     }
 }
