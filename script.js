e0cba0bf5765f8d21401dad9eff2f199bbb4c5b6
Wilczusia <furrythebest200@gmail.com>
2024-11-18 17:50:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.getElementById("submit-btn").addEventListener("click", function() {
     const passwordInput = document.getElementById("password").value;
-    const correctPassword = "𐎘𐎧𐎀𐎩𐎗𐎀𐎚𐎧𐎗𐎌"; // Twoje hasło w starożytnym języku
+    const correctPassword = "𐎘𐎧𐎀𐎩𐎗𐎀𐎚𐎧𐎗𐎌"; // Twoje hasło
 
     if (passwordInput === correctPassword) {
-        // Przekierowanie do strony 'book.html'
+        // Przekierowanie do book.html
         window.location.href = "book.html";
     } else {
-        alert("Nic sie nie stało");
+        alert("Niepoprawne hasło!");
     }
 });
