0b43ddc732e8be588aea701e0638bf3efc9b1432
Wilczusia <furrythebest200@gmail.com>
2024-11-18 17:56:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-document.getElementById("submit-btn").addEventListener("click", function() {
-    const passwordInput = document.getElementById("password").value;
+document.addEventListener("DOMContentLoaded", function() {
+    const submitBtn = document.getElementById("submit-btn");
+    const passwordInput = document.getElementById("password");
+    const bookContent = document.getElementById("book");
+
     const correctPassword = "𐎘𐎧𐎀𐎩𐎗𐎀𐎚𐎧𐎗𐎌"; // Twoje hasło
 
-    if (passwordInput === correctPassword) {
-        // Ukrycie formularza logowania
-        document.querySelector(".login-container").style.display = "none";
-
-        // Ujawnienie zawartości książki
-        document.getElementById("book").style.display = "block";
-    } else {
-        alert("Niepoprawne hasło!");
-    }
+    submitBtn.addEventListener("click", function() {
+        const userPassword = passwordInput.value;
+        if (userPassword === correctPassword) {
+            document.querySelector(".login-container").style.display = "none";
+            bookContent.style.display = "block";
+        } else {
+            alert("Niepoprawne hasło!");
+        }
+    });
 });
